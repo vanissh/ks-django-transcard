90fc4c56c8c0ae3844f43207050cc4a768ca3228
Константин <kostan0008@gmail.com>
2021-07-01 15:16:38 +0300

    v4

--- transcard/transcard/settings.py
+++ transcard/transcard/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ROOT_PATH = os.path.join(BASE_DIR, '')
 
